5f7d08c9b6cfc6a6fe70958ff5527d8fdd13a675
Mahesh M <maheshmuralimvk@gmail.com>
2023-12-26 20:38:37 +0530

    Log In Page Added

---
+++ Webappp/static/js/user.js
@@ -0,0 +1,7 @@
+function signUp(){
+    document.querySelector("#loginForm").classList.add("hidden")
+    document.querySelector("#signupForm").classList.remove("hidden")
+    document.querySelector("#headLogin").classList.add("hidden")
+    document.querySelector("#headSignup").classList.remove("hidden")
+
+}
